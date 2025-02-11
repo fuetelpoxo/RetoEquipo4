ced439a7377e44ef874b19e63eb9d82017e416ea
Angel Fernandez <paratodolodeangel@gmail.com>
2025-02-11 18:10:31 +0100

    navegador
    navegador-prototype

--- FrontEnd/src/components/AppMenu.jsx
+++ FrontEnd/src/components/AppMenu.jsx
@@ -1,16 +1,36 @@
 import { Link } from "react-router-dom";
+import 'font-awesome/css/font-awesome.min.css';
+import './AppMenu.css';
 
 function AppMenu() {
-    return (<nav className="navegacion">
-        <ul>
-            <li><Link to="/">Inicio</Link></li>
-            <li><Link to="/torneo">Torneo</Link></li>
-            <li><Link to="/retos">Retos</Link></li>
-            <li><Link to="/equipos">Equipos</Link></li>
-            <li><Link to="/reglamento">Reglamento</Link></li>
-            <li><Link to="/login">Iniciar Sesion</Link></li>
-        </ul>
-    </nav>);
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <ul className="navbar-nav mx-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link text-white custom-hover" to="/">Inicio</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link text-white custom-hover" to="/torneo">Torneo</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link text-white custom-hover" to="/retos">Retos</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link text-white custom-hover" to="/equipos">Equipos</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link text-white custom-hover" to="/reglamento">Reglamento</Link>
+                    </li>
+                    <li className="nav-item ms-auto">
+                        <Link className="nav-link text-white icon-container" to="/login">
+                            <i className="fa fa-user"></i>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
 export default AppMenu;
